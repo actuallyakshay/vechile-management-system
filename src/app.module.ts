257fff7b82d834b531f4c2bc4a51ab921bb39aad
Akshay Rajput <akshay@fleapo.com>
2025-06-03 12:15:03 +0530

    minor changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,9 @@ import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
 import { validateConfig } from './config.validator';
 import { ConfigModule } from '@nestjs/config';
-import { UploadDataModule } from './upload-data/upload-data.module';
-import { QueryModule } from './query/query.module';
 
 @Module({
-   imports: [ConfigModule.forRoot({ isGlobal: true, validate: validateConfig }), DataModule, UploadDataModule, QueryModule],
+   imports: [ConfigModule.forRoot({ isGlobal: true, validate: validateConfig }), DataModule],
    controllers: [AppController],
    providers: [AppService]
 })
